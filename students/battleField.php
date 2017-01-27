0c7b18f7b1a7e3de6d07c65a2147513ea4c1e83b
Julien BOUSICAUX <Julien.BOUSICAUX@Digital-Grenoble.local>
2017-01-27 16:22:31 +0100

    first commit exo warrior mvc

--- students/battleField.php
+++ students/battleField.php
@@ -4,8 +4,27 @@
   require_once "warrior.php";
 
 
-  class BattleField extends BaseBattleField
-  {
+class BattleField extends BaseBattleField
+ {
+  	public static function createMyWarrior(){
+
+  	$monGuerrier= new StartrekWarrior($GLOBALS['warriorID']);
+  	$monGuerrier->SetImageUrl("http://www.haterz.fr/degenerappeur/files/artiste/le-roi-heenok-2.jpg");
+  	$armes=new Weapon("gourdin", 500);
+  	$armes->SetImageUrl('http://m-y-d-s.com/en/weapons/cudgel/a.jpg');
+  	$monGuerrier->SetWeapon($armes);
+  	$monGuerrier->saveNew();
+
+  	}
+
+	public static function createOtherWarrior(){
+	$maGuerriere= new MarvelWarrior ('lagertha');
+	$maGuerriere-> SetImageUrl("https://pbs.twimg.com/profile_images/501477670778331136/5JP_CLSg_400x400.jpeg");
+	$sword= new Weapon("sword",100); 
+	$sword-> SetImageUrl("https://sitamnesty.files.wordpress.com/2010/07/valeur_sure_epee_viking-ulfberht_kult-of-athena.jpg?w=578&h=238");
+	$maGuerriere->SetWeapon($sword);
+	$maGuerriere->saveNew();
+	}
 
 
-  }
+}
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,113 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'RoxPOneyBaobab';//Modifiez la variable globale warriorID dans le fichier students/warrior.php
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
 
+	public $id;
+	public $name;
+	public $speed;
+	public $life;
+	public $shield;//La classe Warrior doit avoir les attributs $id, $name, $speed, $life et $shield, $imageUrl et $weapon
+	public $imageUrl;
+	public $weapon;
+
+	public function __construct($id){//Les classes Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des constructeurs
+		$this->id= $id;//8/ Le constructeur de Warrior doit prendre en paramètre un $id et initialiser l'id, speed=30, life=100, shield=20
+		$this->speed = 30;
+		$this->life = 100;
+		$this->shield = 20;
+	}
+
+	public function SetWeapon($weapon){ //La classe Warrior doit avoir une méthode SetWeapon() qui prend comme argument un Weapon
+		$this->weapon=$weapon;
+		}
+	public function SetImageUrl($url)
+	{
+			$this->imageUrl=$url;
+	}
 };
+
+
+class StartrekWarrior extends Warrior{//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être crées
+	//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent hériter de Warrior
+	public $mentalPower;
+	//Les classe StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les attributs $mentalPower, $superPower et $level
+	
+
+	public function __construct($id){
+			parent::__construct($id);
+		  	$this->mentalPower=8;
+		 	
+			
+	}	
+
+	function power()//6/ Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir un méthode power qui retournent respectivement $mentalPower, $superPower et $level
+  	{
+   		return $this->mentalPower;
+  	}
+
+}
+
+
+class MarvelWarrior extends Warrior{//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être crées
+	//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent hériter de Warrior 
+	
+	public $superPower ;
+	
+
+	public function __construct($id){
+			
+			parent::__construct($id);
+		 	$this->superPower=100;
+		 	
+		 
+	}
+   		
+	function power()//6/ Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir un méthode power qui retournent respectivement $mentalPower, $superPower et $level
+  	{
+    		return $this->superPower;
+  	}
+}
+
+class PokemonWarrior extends Warrior{//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être crées
+	//Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent hériter de Warrior
+	
+	public $level;
+
+	public function __construct($id){//Les constructeurs des sous classes de warrior doivent appeler le constructeur de Warrior et affecter $mentalPower = 8, $superPower = 100, $level = 1
+			//appel du parametre de la function __construct de la class Warrior  .
+			parent::__construct($id);
+		 	$this->level =1;
+		 	
+
+	}
+   		
+	function power()//6/ Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir un méthode power qui retournent respectivement $mentalPower, $superPower et $level
+  	{
+    return $this->level;
+  	}
+}
+
+
+class Weapon {
+
+	public $id;
+	public $strength;  
+	public $imageUrl;
+
+
+	public function __construct($id, $strength){
+
+
+		$this->id=$id;
+		$this->strength=$strength;
+	}	
+
+	public function SetImageUrl($url)
+	{   //14/ Warrior et Weapon doivent avoir une méthode SetImageUrl($url)
+			$this->imageUrl=$url;
+	}
+}
